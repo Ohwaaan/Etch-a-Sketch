721277676c80a279bc90cbbb7a3b7bf1e9416eca
Owen Lian <owenlian88@gmail.com>
2024-04-29 19:45:04 -0400

    im just a genious

--- sketch.js
+++ sketch.js
@@ -6,9 +6,14 @@ function con(){
 
 grids.addEventListener('click', con)
 
-for(let i =0;i<5;i++)
+for(let i =0;i<6;i++)
 {
     let grid = document.createElement('div');
     grids.appendChild(grid).className = 'gridRow';
+    for( let  i=0;i<4;i++){
+        let cell = document.createElement('div');
+        grid.appendChild(cell).className = 'cell';
+    }
 }
+
 
