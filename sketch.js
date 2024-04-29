6257dca45fbb6995973f82098d5f64296c47b808
Owen Lian <owenlian88@gmail.com>
2024-04-29 19:26:37 -0400

    testing grid system

--- sketch.js
+++ sketch.js
@@ -6,15 +6,9 @@ function con(){
 
 grids.addEventListener('click', con)
 
-let grid = document.createElement('div');
-grid.style.width  = '100px';
-grid.style.height = '100px';
-grid.style.backgroundColor = 'pink';
-grid.innerHtml = 'hell';
-grid.textContent = 'ehll';
-grid.style.borderColor = "black";
-grids.appendChild(grid)
+for(let i =0;i<5;i++)
+{
+    let grid = document.createElement('div');
+    grids.appendChild(grid).className = 'gridRow';
+}
 
-const content = document.createElement('div');
-content.textContent = resultMessage;
-results.appendChild(content);
