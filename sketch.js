eaa46ac702aab1d18768f6494852710ecdffb87e
Ohwaaan <owenlian88@gmail.com>
2024-05-01 05:54:31 -0400

    organized js, added button, color on mouse over

--- sketch.js
+++ sketch.js
@@ -1,31 +1,45 @@
-let grids = document.querySelector('.grids')
+createGrid;
+createPrompt;
+start;
+deleteNodes;
 
-function con(){
-    console.log('workded');
+let grids = document.querySelector('.grids');
+let gridButton = document.querySelector('.gridButton');
+let amountOfGrids;
+
+gridButton.addEventListener('click', start);
+
+function createGrid(){
+
+    if(grids.firstChild)
+        deleteNodes();
+
+    for(let i =0;i<amountOfGrids;i++)
+    {
+        let grid = document.createElement('div');
+        grids.appendChild(grid).className = 'gridRow';
+        for( let  i=0;i<amountOfGrids;i++){
+            let cell = document.createElement('div');
+            grid.appendChild(cell).className = 'cell';
+            cell.querySelector('cell');
+            cell.addEventListener('mouseover', () => cell.style.backgroundColor = 'red')
+        }
+    };
 }
 
-grids.addEventListener('clic', con)
+function createPrompt(){
+    amountOfGrids = prompt('Enter amount of grids!');
+    while(amountOfGrids > 100 || amountOfGrids < 0 || isNaN(amountOfGrids))
+        amountOfGrids = prompt('Enter a number between 0 and 100');
+}
 
+function start(){
+    createPrompt();
+    createGrid();
+}
 
-for(let i =0;i<16;i++)
-{
-    let grid = document.createElement('div');
-    grids.appendChild(grid).className = 'gridRow';
-    for( let  i=0;i<16;i++){
-        let cell = document.createElement('div');
-        grid.appendChild(cell).className = 'cell';
-        cell.querySelector('cell');
-        cell.addEventListener('mouseover', () => cell.style.backgroundColor = 'red')
+function deleteNodes(){
+    while (grids.firstChild){
+        grids.removeChild(grids.firstChild);
     }
-};
-
-function changeColor(){
-    cell.style.backgroundColor = 'red';
 }
-
-/*
-let cell = document.querySelectorAll('.cell');
-cell[1].style.backgroundColor='blue';
-cell.addEventListener('click', con);
-
-for */
