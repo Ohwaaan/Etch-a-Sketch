8b851c3904be8624193955488eeb3d89a2efbad0
Ohwaaan <owenlian88@gmail.com>
2024-04-30 21:56:56 -0400

    loop append classname, mouseover red

--- sketch.js
+++ sketch.js
@@ -4,16 +4,28 @@ function con(){
     console.log('workded');
 }
 
-grids.addEventListener('click', con)
+grids.addEventListener('clic', con)
 
-for(let i =0;i<6;i++)
+
+for(let i =0;i<16;i++)
 {
     let grid = document.createElement('div');
     grids.appendChild(grid).className = 'gridRow';
-    for( let  i=0;i<4;i++){
+    for( let  i=0;i<16;i++){
         let cell = document.createElement('div');
         grid.appendChild(cell).className = 'cell';
+        cell.querySelector('cell');
+        cell.addEventListener('mouseover', () => cell.style.backgroundColor = 'red')
     }
+};
+
+function changeColor(){
+    cell.style.backgroundColor = 'red';
 }
 
+/*
+let cell = document.querySelectorAll('.cell');
+cell[1].style.backgroundColor='blue';
+cell.addEventListener('click', con);
 
+for */
